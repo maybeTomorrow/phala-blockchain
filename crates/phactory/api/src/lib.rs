75a5b08014c208988fb3ed42a9230d1099abad26
Kevin Wang <wy721@qq.com>
2021-08-16 12:38:39 +0800

    phactory-api: Remove unused SIG_LEN

--- crates/phactory/api/src/lib.rs
+++ crates/phactory/api/src/lib.rs
@@ -12,8 +12,6 @@ pub mod prpc {
     use phala_types::messaging::{MessageOrigin, SignedMessage};
     pub use prpc::{client, server, Message};
     pub type EgressMessages = Vec<(MessageOrigin, Vec<SignedMessage>)>;
-
-    pub const SIG_LEN: usize = 64;
 }
 
 #[cfg(feature = "pruntime-client")]
